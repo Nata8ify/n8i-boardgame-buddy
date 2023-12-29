64b221446977740d8811ee0a409d9c85044cc303
nata8ify <nata8ify@gmail.com>
2023-12-29 15:28:17 +0700

    initial commit

---
+++ src/main/java/xyz/n8ify/boardgamebuddy/BoardgameBuddyApplication.java
@@ -0,0 +1,13 @@
+package xyz.n8ify.boardgamebuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardgameBuddyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardgameBuddyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/n8ify/boardgamebuddy/BoardgameBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.n8ify.boardgamebuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardgameBuddyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
